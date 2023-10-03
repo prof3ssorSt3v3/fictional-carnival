750b35ef64c47c946dce35bbfad9f4bc344b4e82
Hennadii Zaiets <zaie000@algonquinlive.com>
2023-10-03 12:35:03 -0400

    Java 2.5 out of 5

---
+++ Documents/mad/git-hubthroughpain/fuzzy-guacamole-group1-section310-main/js/main.js
@@ -0,0 +1,43 @@
+// Group member 2: Your name here
+
+// 1. Convert a number to a string using the toString() method. Print the result to the console.
+
+let num = 10;
+let str = num.toString();
+
+console.log(str); //"10"
+
+// 2. Create an object representing four (4) books with properties like title, author, yearPublished, reviews, and array of genre. Print each property to the console. (Asked in a Goodreads job interview.)
+
+let book1 = {
+    title: "Book1",
+    author: "James",
+    yearPublished: 1990,
+    genre: "comedy"
+}
+
+console.log(book1.title);
+
+console.log(book1.author);
+
+console.log(book1.yearPublished);
+
+console.log(book1.genre);
+
+
+    
+
+// 3. Write a function called calculateArea that calculates and returns the area of a rectangle given its length and width as parameters.
+
+let width = 2;
+let length = 4;
+
+function calculateArea(length, width) {
+    return width * length
+}
+
+// 4.Create a function that accepts a date as a parameter and returns the day of the week for that date.
+
+
+// 5. Declare a boolean variable called isRaining and set it to true if it's currently raining, or false if it's not.
+
